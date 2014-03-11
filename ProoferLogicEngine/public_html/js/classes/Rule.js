484cee0453b8801318f1d5cf1a8a0eddcc28484b
Ryan Kenney <rkenney525@gmail.com>
2014-03-11 16:05:37 -0400

    Added stubs for remaining rules of inference and partially implemented isUnaryOp.

--- ProoferLogicEngine/public_html/js/classes/Rule.js
+++ ProoferLogicEngine/public_html/js/classes/Rule.js
@@ -14,66 +14,88 @@ function Rule(ruleName, displayName, ruleFunction) {
     this.name = ruleName;
     this.displayName = displayName;
     this.applyRule = ruleFunction;
-};
+}
+;
 
 Rule.prototype.getHTML = function() {
     return '<div id="Rule_' + this.displayName + '" class="rule boxed"'
-             + ' ruleid="' + this.displayName + '">' + this.displayName + 
+            + ' ruleid="' + this.displayName + '">' + this.displayName +
             '</div>';
 };
 
 /* Create the list of Rules */
 var Rules = {
-    MP: new Rule("Modus Ponens", "MP", function(arg0, arg1){
-	// Sanity check arg0
-	if (arg0.op !== Operators.COND) {
-	    return null;
-	}
-	
-	/* For example:
-	 *  arg0 = p>q
-	 *  arg1 = p
-	 * Then:
-	 *  arg0.arg0 = p
-	 * And since:
-	 *  arg0.arg0 == arg1 (p == p)
-	 * Return:
-	 *  arg0.arg1 (q)
-	 */
-	if (arg0.arg0.equals(arg1)) {
-	    return arg0.arg1;
-	} else {
-	    return null;
-	}
+    MP: new Rule("Modus Ponens", "MP", function(arg0, arg1) {
+        // Sanity check arg0
+        if (arg0.op !== Operators.COND) {
+            return null;
+        }
+
+        /* For example:
+         *  arg0 = p>q
+         *  arg1 = p
+         * Then:
+         *  arg0.arg0 = p
+         * And since:
+         *  arg0.arg0 == arg1 (p == p)
+         * Return:
+         *  arg0.arg1 (q)
+         */
+        if (arg0.arg0.equals(arg1)) {
+            return arg0.arg1;
+        } else {
+            return null;
+        }
     }),
     MT: new Rule("Modus Tollens", "MT", function(arg0, arg1) {
         // Sanity check arg0
-	if (arg0.op !== Operators.COND) {
-	    return null;
-	}
-        
+        if (arg0.op !== Operators.COND) {
+            return null;
+        }
+
         // Sanity check arg1
-	if (arg1.op !== Operators.NEG) {
-	    return null;
-	}
-        
+        if (arg1.op !== Operators.NEG) {
+            return null;
+        }
+
         /* For example:
-	 *  arg0 = p>q
-	 *  arg1 = ~(q)
-	 * Then:
-	 *  arg0.arg1 = q
+         *  arg0 = p>q
+         *  arg1 = ~(q)
+         * Then:
+         *  arg0.arg1 = q
          *  neg(arg0.arg0) = ~(q)
-	 * And since:
-	 *  neg(arg0.arg1) == arg1 (~(q) == ~(q))
-	 * Return:
-	 *  arg0.arg1 ~(p)
-	 */
+         * And since:
+         *  neg(arg0.arg1) == arg1 (~(q) == ~(q))
+         * Return:
+         *  arg0.arg1 ~(p)
+         */
         var negQ = new Fact(arg0.arg1, null, Operators.NEG);
         if (negQ.equals(arg1)) {
-	    return new Fact(arg0.arg0, null, Operators.NEG);
-	} else {
-	    return null;
-	}
+            return new Fact(arg0.arg0, null, Operators.NEG);
+        } else {
+            return null;
+        }
+    }),
+    DS: new Rule("TODO", "TODO", function(arg0, arg1) {
+
+    }),
+    CD: new Rule("TODO", "TODO", function(arg0, arg1) {
+
+    }),
+    HS: new Rule("TODO", "TODO", function(arg0, arg1) {
+
+    }),
+    Simp: new Rule("TODO", "TODO", function(arg0) {
+
+    }),
+    Conj: new Rule("TODO", "TODO", function(arg0, arg1) {
+
+    }),
+    Abs: new Rule("TODO", "TODO", function(arg0) {
+
+    }),
+    Add: new Rule("TODO", "TODO", function(arg0) {
+
     })
 };
 
@@ -84,7 +106,8 @@ var Rules = {
  * @returns {boolean} True if rule takes only one operand, false otherwise
  */
 function isUnaryRule(rule) {
-    // TODO so far none are so just return false.  Eventually there will be some
-    // and so we will do the check then.
-    return false;
+    // TODO probably have to add rules of replacement
+    return (rule === Rules.Simp) ||
+            (rule === Rules.Abs) ||
+            (rule === Rules.Add);
 }
