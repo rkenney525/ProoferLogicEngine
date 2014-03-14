4915e1adc17307940ff1276e186c500b8a2443fc
Ryan Kenney <rkenney525@gmail.com>
2014-03-14 10:14:26 -0400

    Tutorial framework is in place, just needs more tutorials and to be integrated with the Level class so they appear on level start.

--- ProoferLogicEngine/public_html/js/classes/Tutorial.js
+++ ProoferLogicEngine/public_html/js/classes/Tutorial.js
@@ -1,11 +1,32 @@
-function Tutorial(pages) {
+/**
+ * Creates a Tutorial object. A Tutorial is composed of Pages, which are objects 
+ * that define a visible piece of text and an accomponying subtitle.
+ * 
+ * @param {Page} pages The text and title for each individual slide
+ * @param {String} title The title to give the dialog box
+ * @returns {Tutorial} The Tutorial object created
+ */
+function Tutorial(pages, title) {
     this.pages = pages;
+    this.title = title;
 }
 
-Tutorial.prototype.getHTML = function() {
-    // TODO iterate through the pages and add HTML where necessary
-};
+function Page(content, subtitle) {
+    this.content = content;
+    this.subtitle = subtitle;
+}
 
 var Tutorials = {
-    INTRO: new Tutorial()
+    INTRO: new Tutorial([
+        new Page("<p>Welcome to Proofer - The Logic Engine! Proofer is a game of logical thinking. " +
+                "Soon you will be introduced to a set of rules. These rules allow you to " +
+                "assert certain facts based on other facts that have been proven " +
+                "(or provided to you initially).</p><br /><p>Before we can begin, " +
+                "we will first go over some of the basic types of facts you " +
+                "will encounter, the symbols used in those facts, and what it " +
+                "all means.</p>",
+                "About the Game"),
+        new Page("<p>Test.</p>", "About Facts")
+    ],
+            "Intro")
 };
--- ProoferLogicEngine/public_html/js/dialogs/TutorialDialog.js
+++ ProoferLogicEngine/public_html/js/dialogs/TutorialDialog.js
@@ -1,34 +1,106 @@
 $(document).ready(function() {
     $("#Dialogs_Tutorial").dialog({
-	modal: true,
-	autoOpen: false,
-	buttons: {
-	    "Skip": function() {
-		closeTutorial();
-	    },
-	    "Previous": function() {
-		// TODO implement prev page
-	    },
-	    "Next": function() {
-		// TODO implement next page
-	    }
-	},
-	show: {
-	    effect: "highlight",
-	    duration: 1000
-	},
-	hide: {
-	    effect: "highlight",
-	    duration: 1000
-	}
+        modal: true,
+        autoOpen: false,
+        buttons: {
+            "Skip": function() {
+                closeTutorial();
+            },
+            "Previous": function() {
+                // Init
+                var index = $("#Dialogs_Tutorial")[0].index;
+                var tutorial = $("#Dialogs_Tutorial")[0].tutorial;
+
+                // Change the index
+                $("#Dialogs_Tutorial")[0].index = --index;
+
+                // Append the HTML
+                $("#Dialogs_Tutorial").html(tutorial.pages[index].content);
+
+                // TODO Modify the subtitle
+
+                // Modify the buttons if necessary
+                checkTutorialButtons();
+            },
+            "Next": function() {
+                // Init
+                var index = $("#Dialogs_Tutorial")[0].index;
+                var tutorial = $("#Dialogs_Tutorial")[0].tutorial;
+
+                if (index < (tutorial.pages.length - 1)) {
+                    // Change the index
+                    $("#Dialogs_Tutorial")[0].index = ++index;
+
+                    // Append the HTML
+                    $("#Dialogs_Tutorial").html(tutorial.pages[index].content);
+
+                    // TODO Modify the subtitle
+
+                    // Modify the buttons if necessary
+                    checkTutorialButtons();
+                } else {
+                    closeTutorial();
+                }
+            }
+        },
+        show: {
+            effect: "highlight",
+            duration: 1000
+        },
+        hide: {
+            effect: "highlight",
+            duration: 1000
+        }
     });
 });
 
+function checkTutorialButtons() {
+    // Init
+    var index = $("#Dialogs_Tutorial")[0].index;
+    var tutorial = $("#Dialogs_Tutorial")[0].tutorial;
+
+    // The previous button should be disabled on the first page
+    if (index === 0) {
+        $(".ui-dialog-buttonpane button:contains('Previous')")
+                .attr("disabled", true)
+                .addClass("ui-state-disabled");
+    } else {
+        $(".ui-dialog-buttonpane button:contains('Previous')")
+                .attr("disabled", false)
+                .removeClass("ui-state-disabled");
+    }
+
+    // The Next button should say Close on the last page
+    if (index === (tutorial.pages.length - 1)) {
+        $(".ui-dialog-buttonpane button:contains('Next') span")
+                .filter(function(index) { return $(this).text() === "Next"; }).html("Close");
+    } else {
+        $(".ui-dialog-buttonpane button:contains('Close') span")
+                .filter(function(index) { return $(this).text() === "Close"; }).html("Next");
+    }
+}
+
 function closeTutorial() {
+    // Clear all data related to state
+    $("#Dialogs_Tutorial")[0].tutorial = null;
+    $("#Dialogs_Tutorial")[0].index = 0;
     $("#Dialogs_Tutorial").empty();
     $("#Dialogs_Tutorial").dialog("close");
 }
 
-function openTutorial() {
+function openTutorial(tutorial) {
+    // Set attributes to maintain state
+    $("#Dialogs_Tutorial")[0].tutorial = tutorial;
+    $("#Dialogs_Tutorial")[0].index = 0;
+
+    // Append the HTML
+    $("#Dialogs_Tutorial").html(tutorial.pages[0].content);
+
+    // TODO Modify the title
+
+    // Modify the buttons
+    checkTutorialButtons();
+
+    // Open the dialog
     $("#Dialogs_Tutorial").dialog("open");
 }
