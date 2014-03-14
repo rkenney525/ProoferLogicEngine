ff269b923697012e7db5cfd1415e008438805069
rkenney525 <rkenney525@gmail.com>
2014-03-13 21:15:51 -0400

    Added Level cleared dialog message.  Currently it just displays at start because I'm still testing CSS issues.. like the fact that there appears to be none.

---
+++ ProoferLogicEngine/public_html/js/dialogs/LevelClearedDialog.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    $("#Dialogs_LevelCleared").dialog({
+	modal: true,
+	autoOpen: false,
+	buttons: {
+	    "Next Level": function() {
+		$(this).dialog("close");
+	    },
+	    "Retry": function() {
+		$(this).dialog("close");
+	    }
+	},
+	show: {
+	    effect: "highlight",
+	    duration: 1000
+	},
+	hide: {
+	    effect: "highlight",
+	    duration: 1000
+	}
+    });
+    $("#Dialogs_LevelCleared").dialog("open");
+});
