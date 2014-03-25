e3bf73573ac4fef2f6060f7a4d8aac98ab3e064e
ryan.kenney <ryan.kenney@10.76.130.97>
2014-03-25 15:36:19 -0400

    Added error message

--- ProoferLogicEngine/public_html/js/dialogs/FactCreationDialog.js
+++ ProoferLogicEngine/public_html/js/dialogs/FactCreationDialog.js
@@ -1,80 +1,93 @@
 $(document).ready(function() {
     $("#Dialogs_FactCreation").dialog({
-	modal: true,
-	autoOpen: false,
-	buttons: {
-	    "OK": function() {
-		// Get the fact String and make it parsable
-		var factStr = $('#Dialogs_FactCreation_Creation').text();
+        modal: true,
+        autoOpen: false,
+        buttons: {
+            "OK": function() {
+                // Hide error messages
+                $('#Dialogs_FactCreation_Error_NotEnoughGroups').hide();
+                $('#Dialogs_FactCreation_Error_ElementNotEntered').hide();
 
-		// Look for a few obvious errors
-		var noOps = $('.creation-operator').length === 0;
-		var noParens = factStr.match(/\(/g) === null;
-		var equalParensAndOps = (noParens) ? true : factStr.match(/\(/g).length ===
-			$('.creation-operator').length;
-		if (factStr.indexOf("?") >= 0) {
-		    // TODO display error that says unspecified variable
-		    return;
-		} else if (!noOps && (noParens || !equalParensAndOps)) {
-		    // TODO display error that says not enough groups
-		    return;
-		}
+                // Get the fact String and make it parsable
+                var factStr = $('#Dialogs_FactCreation_Creation').text();
 
-		// Replace the actual operators
-		factStr = factStr.split('\u2192').join('>');
-		factStr = factStr.split('\u2295').join('#');
-		factStr = factStr.split('\u2194').join('%');
-		factStr = factStr.split('\u2227').join('&');
-		factStr = factStr.split('\u2228').join('|');
-		factStr = factStr.split('\u02DC').join('~');
+                // Look for a few obvious errors
+                var noOps = $('.creation-operator').length === 0;
+                var noParens = factStr.match(/\(/g) === null;
+                var equalParensAndOps = (noParens) ? true : factStr.match(/\(/g).length ===
+                        $('.creation-operator').length;
+                if (factStr.indexOf("?") >= 0) {
+                    // Pick the first (or only) unspecified thing
+                    var possiblities = $('.creation-operator, .creation-element');
+                    for (var i = 0; i < possiblities.length; i++) {
+                        if ($(possiblities[i]).text() === "?") {
+                            $('.selected').removeClass('selected');
+                            $(possiblities[i]).addClass('selected');
+                            break;
+                        }
+                    }
+                    $('#Dialogs_FactCreation_Error_ElementNotEntered').show(500);
+                    return;
+                } else if (!noOps && (noParens || !equalParensAndOps)) {
+                    // TODO display error that says not enough groups
+                    return;
+                }
 
-		// Handle the lazy way we displayed negations
-		for (var i = 0; i < factStr.length; i++) {
-		    if (factStr[i] === '~') {
-			var parenCount = 0;
-			for (var j = i + 1; j < factStr.length; j++) {
-			    if (factStr[j] === '(') {
-				parenCount++;
-			    } else if (factStr[j] === ')') {
-				parenCount--;
-			    } else if (factStr[j] === '~') {
-				continue;
-			    }
+                // Replace the actual operators
+                factStr = factStr.split('\u2192').join('>');
+                factStr = factStr.split('\u2295').join('#');
+                factStr = factStr.split('\u2194').join('%');
+                factStr = factStr.split('\u2227').join('&');
+                factStr = factStr.split('\u2228').join('|');
+                factStr = factStr.split('\u02DC').join('~');
 
-			    if (parenCount === 0) {
-				factStr = factStr.insert(j + 1, ')');
-				break;
-			    }
-			}
-			factStr = factStr.insert(i + 1, '(');
-		    }
-		}
+                // Handle the lazy way we displayed negations
+                for (var i = 0; i < factStr.length; i++) {
+                    if (factStr[i] === '~') {
+                        var parenCount = 0;
+                        for (var j = i + 1; j < factStr.length; j++) {
+                            if (factStr[j] === '(') {
+                                parenCount++;
+                            } else if (factStr[j] === ')') {
+                                parenCount--;
+                            } else if (factStr[j] === '~') {
+                                continue;
+                            }
 
-		// Get the fact
-		var fact = getFactFromString(factStr);
+                            if (parenCount === 0) {
+                                factStr = factStr.insert(j + 1, ')');
+                                break;
+                            }
+                        }
+                        factStr = factStr.insert(i + 1, '(');
+                    }
+                }
 
-		// Exit and return
-		if (fact !== null) {
-		    var id = FactCreationDialog_id;
-		    AddTable[id] = fact;
-		    AddTable.updateHtml();
-		    closeFactCreationDialog();
-		} else {
-		    // TODO error message
-		}
-	    },
-	    "Cancel": function() {
-		closeFactCreationDialog();
-	    }
-	},
-	show: {
-	    effect: "highlight",
-	    duration: 1000
-	},
-	hide: {
-	    effect: "highlight",
-	    duration: 200
-	}
+                // Get the fact
+                var fact = getFactFromString(factStr);
+
+                // Exit and return
+                if (fact !== null) {
+                    var id = FactCreationDialog_id;
+                    AddTable[id] = fact;
+                    AddTable.updateHtml();
+                    closeFactCreationDialog();
+                } else {
+                    // TODO error message
+                }
+            },
+            "Cancel": function() {
+                closeFactCreationDialog();
+            }
+        },
+        show: {
+            effect: "highlight",
+            duration: 1000
+        },
+        hide: {
+            effect: "highlight",
+            duration: 200
+        }
     });
 });
 
@@ -96,13 +109,13 @@ function openFactCreationDialog(id, operation) {
     // Check if we have data to load
     clearFactCreation();
     switch (operation) {
-	case "add":
-	    initializeFactCreation();
-	    break;
-	case "edit":
-	    var fact = AddTable[id];
-	    populateFactCreationAreaFromFact(fact);
-	    break;
+        case "add":
+            initializeFactCreation();
+            break;
+        case "edit":
+            var fact = AddTable[id];
+            populateFactCreationAreaFromFact(fact);
+            break;
     }
     FactCreationDialog_id = id;
 
@@ -115,7 +128,13 @@ function openFactCreationDialog(id, operation) {
 
 function closeFactCreationDialog() {
     $('div.ui-dialog.ui-widget.ui-widget-content.ui-corner-all.ui-front.ui-dialog' +
-	    '-buttons.ui-draggable.ui-resizable').unbind("keypress");
+            '-buttons.ui-draggable.ui-resizable').unbind("keypress");
+
+    // Hide error messages
+    $('#Dialogs_FactCreation_Error_NotEnoughGroups').hide();
+    $('#Dialogs_FactCreation_Error_ElementNotEntered').hide();
+
+    // Close
     $("#Dialogs_FactCreation").dialog("close");
 }
 
@@ -131,20 +150,20 @@ function populateFactCreationAreaFromFact(fact) {
      * @param {Fact} fact The Fact to apply
      */
     var populate = function(root, fact) {
-	if (fact.op === null) {
-	    root.append('<span class="creation-element">' + fact.arg0.toString() + '</span>');
-	} else if (fact.arg1 === null) {
-	    var negation = root.append('<span class="creation-negation"></span>');
-	    negation.append('<span class="negation">˜</span>');
-	    populate(negation, fact.arg0);
-	} else {
-	    var group = root.append('<span class="group"></span>');
-	    group.append('<span class="open-paren">(</span>');
-	    populate(group, fact.arg0);
-	    group.append('<span class="creation-operator ui-droppable">' + fact.op.toString() + '</span>');
-	    populate(group, fact.arg1);
-	    group.append('<span class="close-paren">)</span>');
-	}
+        if (fact.op === null) {
+            root.append('<span class="creation-element">' + fact.arg0.toString() + '</span>');
+        } else if (fact.arg1 === null) {
+            var negation = root.append('<span class="creation-negation"></span>');
+            negation.append('<span class="negation">˜</span>');
+            populate(negation, fact.arg0);
+        } else {
+            var group = root.append('<span class="group"></span>');
+            group.append('<span class="open-paren">(</span>');
+            populate(group, fact.arg0);
+            group.append('<span class="creation-operator ui-droppable">' + fact.op.toString() + '</span>');
+            populate(group, fact.arg1);
+            group.append('<span class="close-paren">)</span>');
+        }
     };
     var root = $('#Dialogs_FactCreation_Creation');
     root.empty();
