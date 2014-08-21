7270e7f4d03e67b434bba8484b3f36d5544d30ca
ryan.kenney <ryan.kenney@AFS7II20JX47HO.us.accenturefederal.com>
2014-08-21 15:54:43 -0400

    Added tests for Levels.getCurrentLevel

--- ProoferLogicEngine/public_html/js/tests/Level_tests.js
+++ ProoferLogicEngine/public_html/js/tests/Level_tests.js
@@ -38,3 +38,39 @@ QUnit.test("Level.getHTML", function(assert) {
         testGetHTML(Levels.data[i]());
     }
 });
+
+/*
+ * Test getCurrentLevel method of the Levels object
+ */
+QUnit.test("Levels.getCurrentLevel", function(assert) {
+    // Init
+    var currentLevel;
+    var expectedLevel;
+    
+    // Without save data, should be initialized to 0
+    currentLevel = Levels.getCurrentLevel();
+    expectedLevel = Levels.data[0]();
+    assert.deepEqual(currentLevel, expectedLevel, "Initial Level should be Level 1");
+    
+    // When there is no active level, current level should be initialized
+    Levels.current = null;
+    currentLevel = Levels.getCurrentLevel();
+    expectedLevel = Levels.data[0]();
+    assert.deepEqual(currentLevel, expectedLevel, "When there is no active level, current level should be initialized");
+    
+    // Make sure it loads the Level from the index when there isnt active Level
+    Levels.current = null;
+    Levels.currentIndex = 4;
+    currentLevel = Levels.getCurrentLevel();
+    expectedLevel = Levels.data[4]();
+    assert.deepEqual(currentLevel, expectedLevel, "Load Level from index (when there is no active Level");
+    
+    // If index is changed but data isnt cleared, don't reload a new level
+    Levels.current = null;
+    Levels.currentIndex = 4;
+    currentLevel = Levels.getCurrentLevel();
+    Levels.currentIndex = 5;
+    currentLevel = Levels.getCurrentLevel();
+    expectedLevel = Levels.data[4]();
+    assert.deepEqual(currentLevel, expectedLevel, "If index is changed but data isnt cleared, don't reload a new level");
+});
