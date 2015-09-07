0cb1c4d88e8a9748e756d69d88d3dc981653a19b
Ryan Kenney <rkenney525@gmail.com>
2015-09-06 23:31:54 -0400

    Added more nwjs support, so everything laods except requirejs. The extension no longer works, but working on it.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,5 @@
 module.exports = function (grunt) {
+    // Config
     grunt.initConfig({
         requirejs: {
             compile: {
@@ -9,10 +10,32 @@ module.exports = function (grunt) {
                     out: "build/proofer.js"
                 }
             }
+        },
+        cssmin: {
+            target: {
+                files: {
+                    'build/proofer.css': ['./css/**/*.css']
+                }
+            }
+        },
+        nwjs: {
+            options: {
+                platforms: ['win'],
+                buildDir: 'build',
+                version: 'v0.12.0'
+            },
+            //winIco: "TODO.png"
+            src: ['./package.json', './proofer.html', './build/**/*']
         }
     });
 
+    // Load tasks
     grunt.loadNpmTasks('grunt-contrib-requirejs');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-nw-builder');
 
-    grunt.registerTask('default', ['requirejs']);
+    // Create targets
+    grunt.registerTask('minimize', ['requirejs', 'cssmin']);
+    grunt.registerTask('build', ['nwjs']);
+    grunt.registerTask('default', ['minimize', 'build']);
 };
